044882f78d02048598ba41e9c5ccddc131785252
王杨芃 <wangyangpeng@nongfenqi.com>
2018-05-24 16:34:32 +0800

    修复（epics构建）：修复函数冲突

--- src/web_modules/constants/index.js
+++ src/web_modules/constants/index.js
@@ -1,1 +1,7 @@
 export const NAMESPACE_SEP = "/";
+
+export const NAMESPACE_MAP = {
+    USER: 'user',
+    ADMIN: 'admin',
+    HOME: 'home'
+}
--- src/web_modules/store/index.js
+++ src/web_modules/store/index.js
@@ -1,7 +1,15 @@
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { createEpicMiddleware, combineEpics } from "redux-observable";
 import { routerMiddleware } from "react-router-redux";
-import "./checkModel";
+// import "./checkModel";
+import originalModels from "./allModels";
+import mergeAllModels from './checkModel';
+
+const result = mergeAllModels(...originalModels);
+console.log(result);
+window._newReduce = result.newReduces;
+window._newEpics = result.newEpics;
+window._newActions = result.newActions;
 
 const epicMiddleware = createEpicMiddleware(combineEpics(...window._newEpics));
 
